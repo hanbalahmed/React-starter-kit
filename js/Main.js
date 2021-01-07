27060f2fb8dc216766ae5a9b2e78a3cb48c7936f
Hanbal Ahmed Codes <72215552+hanbalahmed@users.noreply.github.com>
2021-01-07 20:37:09 +0500

    Add files via upload

---
+++ js/Main.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="main__header">
+          <h1>React App</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));

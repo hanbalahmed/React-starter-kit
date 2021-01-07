a33b7cfd75d34bd7e82ece5bb62b6b94f8bee235
Hanbal Ahmed Codes <72215552+hanbalahmed@users.noreply.github.com>
2021-01-07 21:33:56 +0500

    updated styles and JSX

--- js/Main.js
+++ js/Main.js
@@ -4,6 +4,12 @@ class App extends React.Component {
       <div className="main">
         <div className="main__header">
           <h1>React App</h1>
+
+          <br />
+
+          <p style={{ fontSize: 17 }}>
+            Start Editing <code>./js/Main.js</code> to Start
+          </p>
         </div>
       </div>
     );
